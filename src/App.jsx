061714e894551ebc01368408e35b8c2afdf7fa04
Renan <renan.marinhofr@gmail.com>
2023-10-02 16:00:10 -0300

    Estrutura da aplicação - Componente: Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,32 @@
 import { Header } from './Components/Header';
-import { Post } from './post';
+import { Post } from './Post';
 
-import './styles.css';
+import styles from './App.module.css';
+
+import './global.css';
+import { Sidebar } from './components/Sidebar';
+
+
 
 export function App() {
     return (
         <div>
             <Header />
 
-            <Post
-                author="Renan"
-                content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, 
-                minus? Fugit vitae dolorum autem repellat itaque sed aliquam quos saepe, 
-                architecto perferendis molestias qui consequuntur nobis illum dicta facilis ea."
-            />
+            <div className={styles.wrapper}>
+               <Sidebar />
+                <main>
+                    <Post
+                        author="Renan"
+                        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, 
+                                    minus? Fugit vitae dolorum autem repellat itaque sed aliquam quos saepe, 
+                                    architecto perferendis molestias qui consequuntur nobis illum dicta facilis ea."
+                    />
 
-            <Post author="Marinho" content="Um novo post muito legal" />
+                    <Post author="Marinho" content="Um novo post muito legal" />
+                </main>
+            </div>
+
         </div>
     );
 }
