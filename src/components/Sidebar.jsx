f26e4fb7f970795c30ac0af96eb970f4203b6c1b
Renan <renan.marinhofr@gmail.com>
2023-10-03 14:15:48 -0300

    Estrutura da aplicação - Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,6 @@
 import { PencilLine } from 'phosphor-react'
+
+
 // import { Avatar } from './Avatar';
 
 import styles from './Sidebar.module.css';
@@ -26,34 +28,5 @@ export function Sidebar() {
     </aside>
 
 
-
-
-
   )
 }
-
-
-// export function Sidebar() {
-//   return (
-//     <aside className={styles.sidebar}>
-//       <img
-//         className={styles.cover}
-//         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
-//       />
-
-//       <div className={styles.profile}>
-//         <Avatar src="https://avatars.githubusercontent.com/u/91439877?v=4" />
-
-//         <strong>Renan Marinho</strong>
-//         <span>Web Developer</span>
-//       </div>
-
-//       <footer>
-//         <a href="#">
-//           <PencilLine size={20} />
-//           Editar seu perfil
-//         </a>
-//       </footer>
-//     </aside>
-//   );
-// }
